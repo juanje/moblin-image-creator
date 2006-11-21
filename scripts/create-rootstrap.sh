c173d80b1b26c0fc01a700490b99f6f236b624d4
Rusty Lynch <rusty@stratocaster.stinkycat.com>
2006-11-21 11:02:04 -0800

    Initial import of the create-rootstrap script

---
+++ scripts/create-rootstrap.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+###############################################################################
+# create-rootstrap.sh : Creates a basic rootstrap package for consumption
+#                       by scratchbox. 
+###############################################################################
+
+set -e
+
+# absolute path to new directory for creating workspace
+workspace_dir=$1
+
+# URL to yum repository. Note that using a file:/// URL will work in creating
+# the rootstrap, but then yum will not be usable inside the resulting workspace
+# untill either the yum repository is made available in the same absolute
+# location inside the workspace (like by mounting a portion of the filesystem 
+# using the --bind argument), or by changing the baseurl in the yum config.
+url=$2
+
+# top level packages to install in the rootstrap image. Yum will automatically
+# perform dependency resolution
+PACKAGES="util-linux rpm yum udev"
+
+if [ -z "$workspace_dir" ] || [ -z "$url" ]; then
+    echo "USAGE: $0 NEW_WORKSPACE_DIR YUM_URL"
+    exit -1
+fi
+
+if [ -e $workspace_dir ]; then
+    echo "$workspace_dir already exist!"
+    exit -1
+fi
+
+if [ `whoami` != "root" ]; then
+    echo "You have to be root to create a new rootstrap!"
+    exit -1
+fi
+
+# TODO: Either enforce that the workspace_dir is an absolute path
+#       or automatically convert it to an absolute path.  Yum expects
+#       an absolute path for it's rootinstall argument and fails in
+#       a most ungraceful mannor if you pass it anything less.
+
+##########################################################
+## Create a yum configuration that uses the specified URL
+##########################################################
+
+mkdir -p $workspace_dir/etc/
+cat > $workspace_dir/etc/yum.conf <<EOF
+[main]
+cachedir=/var/cache/yum
+reposdir=/etc/yum.repos.d
+debuglevel=2
+errorlevel=2
+logfile=/var/log/yum.log
+gpgcheck=0
+assumeyes=0
+tolerant=1
+exactarch=1
+obsoletes=1
+distroverpkg=suse-release
+retries=20
+pkgpolicy=newest
+EOF
+
+mkdir $workspace_dir/etc/yum.repos.d/
+cat > $workspace_dir/etc/yum.repos.d/base.repo <<EOF
+[sled10]
+name=SLED 10
+baseurl=$url
+enabled=1
+gpgcheck=0
+EOF
+
+##########################################################
+## Final fixup before calling yum
+##########################################################
+
+# If we don't create /dev/null then various packages 
+# will waste their time redirecting thier output to a real file
+# located at /dev/null
+mkdir -p $workspace_dir/dev/
+mknod $workspace_dir/dev/null c 1 3
+
+# Carry over the creators basic system configuration
+cp /etc/hosts $workspace_dir/etc/
+cp /etc/passwd $workspace_dir/etc/
+cp /etc/group $workspace_dir/etc/
+
+mkdir -p $workspace_dir/proc/
+
+##########################################################
+## Install the minimal set workspace packages
+##########################################################
+mkdir -p $workspace_dir/var/lib/rpm
+yum -y --installroot=$workspace_dir install $PACKAGES
+
+##########################################################
+## Perform final fixup on the workspace
+##########################################################
+
+# Create a minimal /dev
+cp --preserve=all --recursive --update $workspace_dir/lib/udev/devices/* $workspace_dir/dev
+
+##########################################################
+## Package the workspace into a rootstrap archive
+##########################################################
+
+rm -f rootstrap.tgz
+tar -zcpvf rootstrap.tgz -C $workspace_dir .
+
