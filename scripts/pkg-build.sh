44965bc585e580d15dee77d4609a614a1c7edcb9
Rob Rhoads <rob.rhoads@intel.com>
2007-02-07 16:58:11 -0800

    Updated the scripts/pkg-build.sh script to support spec files w/ more than one sub-package.

--- scripts/pkg-build.sh
+++ scripts/pkg-build.sh
@@ -82,7 +82,7 @@ if [ -z "$spec_file" ]; then
 fi
 
 spec_pkg_name=`awk '/^Name:/ {print $2}' $spec_file`
-pkg_group=`awk '/^Group:/ {print $2}' $spec_file`
+pkg_group=`grep -m 1 ^Group: ${spec_file} | awk '{print $2}'`
 
 # Sanity check the spec file vs. package name
 if [ "$spec_pkg_name" != "$pkg_name" ] ; then
