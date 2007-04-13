792c3404f22c7c20a7b4f8baa57e97784827acc1
John L. Villalovos <john.l.villalovos@intel.com>
2007-04-13 12:34:53 -0700

    more work on unit tests

--- esdk/unittest/testPlatform.py
+++ esdk/unittest/testPlatform.py
@@ -9,25 +9,44 @@ import Platform
 class TestPlatform(unittest.TestCase):
     def setUp(self):
         self.workdir = tempfile.mkdtemp()
-        self.platform_dir = os.path.join(self.workdir, 'platforms', 'unittest')
-        createSamplePlatformDir(self.platform_dir)
+        self.platform_name = 'unittest_platform'
+        self.platform_dir = os.path.join(self.workdir, 'platforms', self.platform_name)
+        self.repo_filename = 'unittest.repo'
+        createSamplePlatformDir(self.platform_dir, self.repo_filename)
     def tearDown(self):
         if os.path.isdir(self.workdir):
             shutil.rmtree(self.workdir)
-    def testInstantiate(self):
-        platform = Platform.Platform(self.workdir, 'unittest')
+    def test001Instantiate(self):
+        """Simple instantiation test"""
+        platform = Platform.Platform(self.workdir, self.platform_name)
     def testStrRepr(self):
-        platform = Platform.Platform(self.workdir, 'unittest')
+        """Test __str__ and __repr__ functions"""
+        platform = Platform.Platform(self.workdir, self.platform_name)
         temp = platform.__str__()
         temp = platform.__repr__()
+    def testValues(self):
+        """Test storing and retrieving values"""
+        platform = Platform.Platform(self.workdir, self.platform_name)
+        # Make sure it is storing what we passed in
+        self.assertEqual(platform.sdk_path, self.workdir)
+        self.assertEqual(platform.name, self.platform_name)
+    def testRepoFiles(self):
+        """Test correctness of repository files"""
+        platform = Platform.Platform(self.workdir, self.platform_name)
+        # Test buildroot_repos
+        self.assertEqual(len(platform.buildroot_repos), 1)
+        self.assertEqual(platform.buildroot_repos[0], os.path.join(self.platform_dir, 'buildroot_repos', self.repo_filename))
+        # Test target_repos
+        self.assertEqual(len(platform.target_repos), 1)
+        self.assertEqual(platform.target_repos[0], os.path.join(self.platform_dir, 'target_repos', self.repo_filename))
 
-def createSamplePlatformDir(platform_dir):
+def createSamplePlatformDir(platform_dir, repo_filename = "unittest.repo"):
     os.makedirs(platform_dir)
     for dirname in ('buildroot_repos', 'fsets', 'target_repos'):
         os.mkdir(os.path.join(platform_dir, dirname))
     testFSet.createSampleFsetFile(os.path.join(platform_dir, 'fsets', 'unittest.fset'))
     createSampleJailrootPackages(os.path.join(platform_dir, 'jailroot.packages'))
-    createSampleRepos(platform_dir)
+    createSampleRepos(platform_dir, repo_filename)
 
 def createSampleJailrootPackages(filename):
     contents = """\
@@ -45,7 +64,7 @@ syslinux busybox squashfs-tools
 """
     text2file(filename, contents)
 
-def createSampleRepos(root_dir):
+def createSampleRepos(root_dir, repo_filename):
     repo_text = """\
 [esdk-base]
 name=ESDK Base
@@ -71,7 +90,7 @@ baseurl=http://umd-build2.jf.intel.com/yum-repos/mid-core/
 enabled=1
 gpgcheck=0"""
     for dirname in ('buildroot_repos', 'target_repos'):
-        full_path = os.path.join(root_dir, dirname, "unittest.repo")
+        full_path = os.path.join(root_dir, dirname, repo_filename)
         text2file(full_path, repo_text)
 
 
@@ -81,4 +100,8 @@ def text2file(filename, text):
     out_file.close()
 
 if __name__ == '__main__':
-    unittest.main()
+    if len(sys.argv) == 2 and sys.argv[1] == "-v":
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestPlatform)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+    else:
+        unittest.main()
