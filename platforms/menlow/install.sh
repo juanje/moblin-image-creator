5dd9ecbdd5e6f1fa15cb87b8c17e445ce3a15a3c
Rusty Lynch <rusty.lynch@intel.com>
2007-07-11 04:53:01 -0700

    * Updating menlow for latest changes made in mccaslin

---
+++ platforms/menlow/install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo -e '\nDeleting Partition Table on /dev/hda...\n'
+dd if=/dev/zero of=/dev/hda bs=512 count=2
+sync
+
+echo -e 'Creating New Partiton Table on /dev/hda...\n'
+fdisk /dev/hda <<EOF
+n
+p
+1
+1
++1024M
+n
+p
+2
+
+
+a
+1
+w
+EOF
+
+sync
+
+echo -e 'Formatting /dev/hda1 w/ ext2...\n'
+mkfs.ext2 /dev/hda1
+sync
+
+echo -e 'Formatting /dev/hda2 w/ ext3...\n'
+mkfs.ext3 /dev/hda2
+sync
+
+echo -e 'Mounting partitions...\n'
+mkdir /tmp/boot
+mount -o loop -t squashfs /tmp/install/bootfs.img /tmp/boot
+
+mount /dev/hda2 /mnt
+mkdir /mnt/boot
+mount /dev/hda1 /mnt/boot
+
+echo -e 'Copying system files onto hard disk drive...\n'
+cp -v /tmp/install/rootfs.img /mnt/boot
+cp -av /tmp/boot /mnt
+
+/usr/sbin/grub-install --root-directory=/mnt /dev/hda
+
+echo -e 'Unmounting partitions...\n'
+umount /mnt/boot
+umount /mnt
+umount /tmp/boot
+umount /tmp/install
+
+echo -e '\n\n\nInstall Finished!\n\n'
+echo -e 'Disconnect the USB-Key and power cycle the device\n'
+
+while true; do
+    sleep 100
+done
+
+
