8b239fa32fb65b467e3681023fe656a14b8b1fd2
John L. Villalovos <john.l.villalovos@intel.com>
2007-07-23 20:11:26 -0700

    * Fix Bug 25

--- libs/Project.py
+++ libs/Project.py
@@ -200,8 +200,12 @@ class Project(FileSystem):
                 os.system('touch %s/etc/kernel-pkg.conf' % (install_path))
                 
                 for f in os.listdir(os.path.join(self.platform.path, 'sources')):
-                    shutil.copy(os.path.join(self.platform.path, 'sources', f), os.path.join(install_path, 'etc', 'apt', 'sources.list.d'))
-                    shutil.copy(os.path.join(self.platform.path, 'preferences'), os.path.join(install_path, 'etc', 'apt'))
+                    source_path = os.path.join(self.platform.path, 'sources', f)
+                    if os.path.exists(source_path):
+                        shutil.copy(source_path, os.path.join(install_path, 'etc', 'apt', 'sources.list.d'))
+                    source_path = os.path.join(self.platform.path, 'preferences')
+                    if os.path.exists(source_path):
+                        shutil.copy(source_path, os.path.join(install_path, 'etc', 'apt'))
                 if use_rootstrap:
                     cmd = "tar -jcpvf %s -C %s ." % (rootstrap, install_path)
                     output = []
