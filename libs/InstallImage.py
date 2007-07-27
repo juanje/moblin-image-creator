e03003ff7e1fbaf20b18e76e9f0ea82c597a8ae1
Alek Du <alek.du@intel.com>
2007-07-27 17:38:38 +0800

    umount target fs's /proc & /sys file system before making squashfs, otherwize mksquash will encounter errors, since there are now not bind mounted. :-(

--- libs/InstallImage.py
+++ libs/InstallImage.py
@@ -199,7 +199,9 @@ class InstallImage(object):
         image_path   = os.path.join(image_path,'rootfs.img')
         cmd_args     = "%s %s -ef %s" % (fs_path, image_path, self.exclude_file)
 
+        self.target.umount()
         self.project.chroot("/usr/sbin/mksquashfs", cmd_args)
+        self.target.mount()
             
     def delete_rootfs(self):
         if self.rootfs and os.path.isfile(self.rootfs_path):
