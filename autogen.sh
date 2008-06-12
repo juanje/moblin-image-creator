47d39f8a13b526337126eda2334a958f6ad6a370
Mitsutaka Amano <mamano@miraclelinux.com>
2008-06-12 13:14:14 +0900

    Updated README to modified installation process.

--- autogen.sh
+++ autogen.sh
@@ -5,3 +5,4 @@ intltoolize --force || exit 1
 aclocal -I m4 || exit 1
 autoconf || exit 1
 automake --add-missing --foreign || exit 1
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var || exit 1
